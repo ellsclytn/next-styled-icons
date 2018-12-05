c0d0e43323de1cb43d0269dbcef932ad179664e0
Ellis Clayton <ellis@ellisclayton.info>
2018-12-05 12:55:44 +1100

    Add custom Styled Icon
    Demonstrates the className mismatch between client and server when a hot
    reload occurs.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { Search } from "styled-icons/material/Search";
+import { Search } from "styled-icons/material";
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `;
 
+const MyIcon = styled(Search)`
+  color: red;
+  height: 24px;
+`;
+
 export default () => (
   <div>
-    <Search />
+    <p>Toggle commenting this line to see the error</p>
+    <MyIcon />
+    <p>Hi there</p>
     <Title>My page</Title>
   </div>
 );
